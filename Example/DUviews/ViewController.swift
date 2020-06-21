8e834fcc8128f8efb603cf3e382fb073aec0efba
mohamed saeed <m.saeed@planradar.com>
2020-06-21 18:17:58 +0200

    #runing pod & example

--- Example/DUviews/ViewController.swift
+++ Example/DUviews/ViewController.swift
@@ -7,18 +7,28 @@
 //
 
 import UIKit
-
+import DUviews
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupUI() {
+        let label = DULabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 200), parent: nil, font: .systemFont(ofSize: 44), textColor: .black)!
+        label.numberOfLines = 0
+        label.textAlignment  = .center
+        label.text = """
+        let label = DULabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100), parent: nil, font: .latoBold(size: 31), textColor: .blue)
+           let label = DULabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100), parent: nil, font: .latoBold(size: 31), textColor: .blue)
+           let label = DULabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100), parent: nil, font: .latoBold(size: 31), textColor: .blue)
+           let label = DULabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100), parent: nil, font: .latoBold(size: 31), textColor: .blue)
+           let label = DULabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100), parent: nil, font: .latoBold(size: 31), textColor: .blue)
+           let label = DULabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100), parent: nil, font: .latoBold(size: 31), textColor: .blue)
+        """
+        let scrollView = DUSingleChildScrollView(frame: self.view.frame, child: label, parent: self.view)
+        scrollView.backgroundColor = .white
+        
     }
-
 }
-
